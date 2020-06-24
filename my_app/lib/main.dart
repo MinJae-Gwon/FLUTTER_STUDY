3427e31c891fce9df6eaa2428f4daac402b2c108
MinJaeGwon <minjae2271@gmail.com>
2020-06-24 22:54:19 +0900

    chart

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './widgets/chart.dart';
 import 'package:my_app/widgets/new_transactions_prac.dart';
 import './widgets/transaction_list_prac.dart';
 import './models/transaction.dart';
@@ -33,19 +34,25 @@ class MyHomePage extends StatefulWidget {
 
 class _MyAppState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    // Transaction(
-    //   id: 't1',
-    //   title: 'Amazon',
-    //   amount: 69.00,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: 't2',
-    //   title: 'Coupang',
-    //   amount: 12.00,
-    //   date: DateTime.now(),
-    // ),
+    Transaction(
+      id: 't1',
+      title: 'Amazon',
+      amount: 69.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Coupang',
+      amount: 12.00,
+      date: DateTime.now(),
+    ),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
 
   void _addNewTransaction(String title, double amount) {
     final newTrx = Transaction(
@@ -90,6 +97,7 @@ class _MyAppState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            Container(child: Chart(_recentTransactions),),
             Container(
               child: TransactionList(_userTransactions),
             ),
